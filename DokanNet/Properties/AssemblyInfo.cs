f4dfbd9050c72ae7ab047b745718dc67b0f1834a
Me <quandary82@hailmail.net>
2015-01-08 10:17:27 +0100

    Cleanup samples

--- DokanNet/Properties/AssemblyInfo.cs
+++ DokanNet/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2015.1.7.1647")]
-[assembly: AssemblyFileVersion("2015.1.7.1647")]
+[assembly: AssemblyVersion("2015.1.8.1016")]
+[assembly: AssemblyFileVersion("2015.1.8.1016")]
